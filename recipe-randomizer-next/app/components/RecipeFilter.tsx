98d7ccadc76c7a4663116c8741aa55e9318ade0e
Joseph Villanueva <josephgvillanueva24@gmail.com>
2024-10-12 21:31:05 +0800

    adjust styling and comments

--- recipe-randomizer-next/app/components/RecipeFilter.tsx
+++ recipe-randomizer-next/app/components/RecipeFilter.tsx
@@ -1,27 +1,27 @@
 "use client";
 
 import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { HiInformationCircle } from "react-icons/hi";
 
 interface RecipeFilterProps {
   fetchRecipes: (ingredients: string) => void;
   clearRecipes: () => void;
 }
 
-// RecipeFilter component to handle user input for recipe search
 const RecipeFilter: React.FC<RecipeFilterProps> = ({
   fetchRecipes,
   clearRecipes,
 }) => {
   const [inputValue, setInputValue] = useState("");
+  const [showTooltip, setShowTooltip] = useState(false);
 
-  // Handle search when Enter is pressed
   const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       fetchRecipes(inputValue);
     }
   };
 
-  // Clear input and recipes
   const handleClear = () => {
     setInputValue("");
     clearRecipes();
@@ -29,14 +29,33 @@ const RecipeFilter: React.FC<RecipeFilterProps> = ({
 
   return (
     <div className="flex flex-col mb-4">
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyDown={handleSearch}
-        placeholder="Enter ingredients..."
-        className="p-2 border rounded"
-      />
+      <div className="relative">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleSearch}
+          placeholder="Enter ingredients..."
+          className="p-2 border rounded w-full"
+        />
+        <div className="absolute top-0 right-0 mt-2 mr-2">
+          <span
+            className="text-gray-500 text-xs cursor-pointer"
+            onMouseEnter={() => setShowTooltip(true)}
+            onMouseLeave={() => setShowTooltip(false)}
+          >
+            <HiInformationCircle className="hover:text-blue-500 transition-colors duration-300 text-xl" />{" "}
+            <motion.span
+              className={`tooltip ${showTooltip ? "show-tooltip" : ""}`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: showTooltip ? 1 : 0 }}
+              transition={{ duration: 0.2 }}
+            >
+              Separate ingredients with a comma (e.g., bacon, egg)
+            </motion.span>
+          </span>
+        </div>
+      </div>
       <div className="flex justify-between mt-2">
         <button
           onClick={() => fetchRecipes(inputValue)}
